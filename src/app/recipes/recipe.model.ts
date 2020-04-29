79071ec398cfe4df2f81a97a07809cffb34ff987
Jeff Bryan <jeffbryan91@gmail.com>
2020-04-29 12:34:33 -0400

    Basic Html Setup

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imageUrl: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imageUrl;
+    }
+}
